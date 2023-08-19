6e74e86cf2437df3904db8c27f762185de7d8ec6
mrwindupbirdesq <nmaahs11@gmail.com>
2023-08-19 12:27:10 -0700

    Serve custom html for 404

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ http
       : (filename = '.' + q.pathname);
     fs.readFile(filename, function (err, data) {
       if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end('404 Not Found');
+        fs.readFile('404.html', function (err, data) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.write(data);
+          res.end();
+        });
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
     });
   })
   .listen(8080);
