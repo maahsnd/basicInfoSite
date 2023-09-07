a3ce08d063645716d9191d2a7fedce30630acd86
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-07 10:13:49 -0700

    Refactor express

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-http
-  .createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename;
-    /// if no pathname, serve index, else serve pathname
-    q.pathname === '/'
-      ? (filename = 'index.html')
-      : (filename = '.' + q.pathname);
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        fs.readFile('404.html', function (err, data) {
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          res.write(data);
-          res.end();
-        });
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      }
-    });
-  })
-  .listen(8080);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(3000);
